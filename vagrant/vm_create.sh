538daa585d92d92ab7b09b0323c26cd4c25cc681
乞丐亦异 <haibin.lee@foxmail.com>
2025-01-20 02:08:08 +0000

    增强 Vagrant 虚拟机配置和脚本功能：安装 telnet，添加网络接口设置，扩展 vm_create.sh 脚本支持更多操作

--- vagrant/vm_create.sh
+++ vagrant/vm_create.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
-vagrant destroy -f
-vagrant up
-vagrant snapshot save init
+case "$1" in
+    destroy)
+        vagrant destroy -f
+    ;;
+    snapshot)
+        vagrant snapshot save $2
+        ;;
+    restore)
+        vagrant snapshot restore $2
+        ;;
+    init)
+        vagrant snapshot restore init
+        ;;
+    rebuilt)
+        vagrant destroy -f
+        vagrant up
+        vagrant snapshot save init
+        ;;
+    *)
+        echo "Usage: $0 {destroy|snapshot|restore|init}"
+        exit 1
+esac
